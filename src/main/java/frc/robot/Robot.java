a4824d8ba45ed8b18381971641a5eb6741779dab
frc-5728 <114952840+frc-5728@users.noreply.github.com>
2023-03-21 12:37:06 -0700

    compressor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,18 +4,23 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
+  private final Compressor compressor = new Compressor(PneumaticsModuleType.REVPH);
 
   private RobotContainer m_robotContainer;
 
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+
+    compressor.enableDigital();
   }
 
   @Override
